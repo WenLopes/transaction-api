778d4db031c64dbd348dde65af9b3179eeac8cb3
Wendel Lopes <wendel.lopes@telium.com.br>
2021-05-27 18:46:47 -0300

    Add createTransactionRequest to controller

--- api/app/Http/Controllers/TransactionController.php
+++ api/app/Http/Controllers/TransactionController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\Transaction\CreateTransactionRequest;
 
 class TransactionController extends Controller
 {
-    public function create(Request $request)
+    public function create(CreateTransactionRequest $request)
     {
+        $data = $request->validated();
+        dd($data);
     }
 }
---
+++ api/app/Http/Requests/Transaction/CreateTransactionRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests\Transaction;
+
+use App\Http\Requests\BaseFormRequest;
+use Illuminate\Foundation\Http\FormRequest;
+
+
+class CreateTransactionRequest extends BaseFormRequest
+{
+    /**
+     * Determine if the User is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'value' => [
+                'required', 
+                'numeric',
+                'min:0.01'
+            ],
+            'payer' => [
+                'required',
+                'integer',
+                'exists:users,id'
+            ],
+            'payee' => [
+                'required',
+                'integer',
+                'exists:users,id',
+                'different:payer'
+            ]
+        ];
+    }
+}
