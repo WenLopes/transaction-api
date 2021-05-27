ce3ded6a0cea51d6579a55386a9f2bcad2ba8c08
Wendel Lopes <wendel.lopes@telium.com.br>
2021-05-27 19:16:05 -0300

    Add Repositories Service Provider in app config

--- api/config/app.php
+++ api/config/app.php
@@ -174,7 +174,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\FakerServiceProvider::class
+        App\Providers\FakerServiceProvider::class,
+        App\Providers\RepositoriesServiceProvider::class
 
     ],
 
