41197077d9a563573811a212cc5f755f83950f5d
Wendel Lopes <wendel.lopes@telium.com.br>
2021-05-27 18:58:25 -0300

    Add base repository with interface

---
+++ api/app/Repositories/BaseRepository.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repositories;
+
+use App\Repositories\BaseRepositoryInterface;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+class BaseRepository implements BaseRepositoryInterface
+{
+    /**
+     * @var Model
+     */
+    protected $model;
+
+    /**
+     * BaseRepository constructor.
+     *
+     * @param Model $model
+     */
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    /**
+     * @param array $columns
+     * @param array $relations
+     * @return Collection
+     */
+    public function all(array $columns = ['*'], array $relations = []): Collection
+    {
+        return $this->model->with($relations)->get($columns);
+    }
+
+    /**
+     * Find model by id.
+     *
+     * @param int $modelId
+     * @param array $columns
+     * @param array $relations
+     * @param array $appends
+     * @return Model
+     */
+    public function findById(
+        int $modelId,
+        array $columns = ['*'],
+        array $relations = []
+    ): ?Model {
+        return $this->model->select($columns)->with($relations)->find($modelId);
+    }
+
+    /**
+     * Create a model.
+     *
+     * @param array $payload
+     * @return Model
+     */
+    public function create(array $payload): ?Model
+    {
+        $model = $this->model->create($payload);
+        return $model->fresh();
+    }
+
+    /**
+     * Update existing model.
+     *
+     * @param int $modelId
+     * @param array $payload
+     * @return bool
+     */
+    public function update(int $modelId, array $payload): bool
+    {
+        $model = $this->findById($modelId);
+
+        return $model->update($payload);
+    }
+}
