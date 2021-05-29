1a3d5e451a44e60da472d8118abc5211a96018b5
Wendel Lopes <wendel.lopes@telium.com.br>
2021-05-29 00:02:17 -0300

    Adding fillable to Notification Model

--- api/app/Models/Notification/Notification.php
+++ api/app/Models/Notification/Notification.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Notification extends Model
 {
     use HasFactory, NotificationRelations;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'notification_status_id',
+        'subject',
+        'content',
+        'dispatched'
+    ];
 }
