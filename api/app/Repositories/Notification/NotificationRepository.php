dcc2145ca99d16962012c0db9dea163728c6ec31
Wendel Lopes <wendel.lopes@telium.com.br>
2021-05-29 16:00:20 -0300

    Creating notification status updaters

--- api/app/Repositories/Notification/NotificationRepository.php
+++ api/app/Repositories/Notification/NotificationRepository.php
@@ -22,4 +22,56 @@ class NotificationRepository extends BaseRepository implements NotificationRepos
     {
         $this->model = $model;
     }
+
+    /**
+     * Set notification status as dispatched
+     * @param int $notificationId
+     * @return bool
+     */
+    public function setAsDispatched( int $notificationId ) : ?Notification {
+
+        $notification = $this->findById($notificationId);
+
+        if( !$notification ){
+            return false;
+        }
+
+        $dispatchedStatus = config('constants.notification.status.DISPATCHED');
+
+        $query = $this->update($notificationId, [
+            'notification_status_id' => $dispatchedStatus
+        ]);
+
+        if( !$query ){
+            return false;
+        }
+
+        return $this->findById($notificationId);
+    }
+
+    /**
+     * Set notification status as error
+     * @param int $notificationId
+     * @return bool
+     */
+    public function setAsError( int $notificationId ) : ?Notification {
+
+        $notification = $this->findById($notificationId);
+
+        if( !$notification ){
+            return false;
+        }
+
+        $errorStatus = config('constants.notification.status.ERROR');
+
+        $query = $this->update($notificationId, [
+            'notification_status_id' => $errorStatus
+        ]);
+
+        if( !$query ){
+            return false;
+        }
+
+        return $this->findById($notificationId);
+    }
 }
--- api/app/Repositories/Notification/NotificationRepositoryInterface.php
+++ api/app/Repositories/Notification/NotificationRepositoryInterface.php
@@ -2,6 +2,13 @@
 
 namespace App\Repositories\Notification;
 
+use App\Models\Notification\Notification;
 use App\Repositories\BaseRepositoryInterface;
 
-interface NotificationRepositoryInterface extends BaseRepositoryInterface {}
+interface NotificationRepositoryInterface extends BaseRepositoryInterface {
+
+    public function setAsDispatched( int $notificationId ) : ?Notification;
+
+    public function setAsError( int $notificationId ) : ?Notification;
+
+}
