4e5581e64959c37cac96c5b25125f78bc93f9ca5
Wendel Lopes <wendel.lopes@telium.com.br>
2021-05-30 12:16:07 -0300

    adding data to transaction resource

--- api/app/Http/Resources/TransactionResource.php
+++ api/app/Http/Resources/TransactionResource.php
@@ -16,11 +16,73 @@ class TransactionResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'payee' => $this->payee_id,
-            'payer' => $this->payer_id,
+            'payee' => $this->getPayeeData(),
+            'payer' => $this->getPayerData(),
             'value' => $this->value,
-            'status' => $this->transaction_status_id,
+            'status' => $this->getStatusData(),
             'created_at' => $this->created_at
         ];
+    }
+
+    /**
+     * Get payee data
+     * @return array
+     */
+    protected function getPayeeData() : array
+    {
+        $payee = [
+            'id' => $this->payee_id
+        ];
+
+        if( isset($this->payee) ){
+            $payee = [
+                'id' => $this->payee->id,
+                'name' => $this->payee->name,
+                'document' => $this->payee->document
+            ];
+        }
+
+        return $payee;
+    }
+
+    /**
+     * Get payer data
+     * @return array
+     */
+    protected function getPayerData() : array
+    {
+        $payer = [
+            'id' => $this->payer_id
+        ];
+
+        if( isset($this->payer) ){
+            $payer = [
+                'id' => $this->payer->id,
+                'name' => $this->payer->name,
+                'document' => $this->payer->document
+            ];
+        }
+
+        return $payer;
+    }
+
+    /**
+     * Get payer data
+     * @return array
+     */
+    protected function getStatusData() : array
+    {
+        $status = [
+            'id' => $this->transaction_status_id
+        ];
+
+        if( isset($this->status) ){
+            $status = [
+                'id' => $this->status->id,
+                'description' => $this->status->description
+            ];
+        }
+
+        return $status;
     }
 }
