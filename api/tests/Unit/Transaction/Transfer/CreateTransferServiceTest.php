530669bdf8f0422a5b51d1553d7dbaeef9c0e777
Wendel Lopes <wendel.lopes@telium.com.br>
2021-05-31 19:31:54 -0300

    Creating unit tests on CreateTransferService class

--- api/tests/Unit/Transaction/Transfer/CreateTransferServiceTest.php
+++ api/tests/Unit/Transaction/Transfer/CreateTransferServiceTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\Transaction\Transfer;
 
 use App\Exceptions\Transaction\Transfer\CreateTransferException;
+use App\Jobs\Transaction\Transfer\ProcessTransferJob;
 use App\Models\Transaction\Transaction;
 use App\Repositories\Transaction\TransactionRepositoryInterface;
 use App\Repositories\User\UserRepositoryInterface;
@@ -10,6 +11,7 @@ use App\Services\Transaction\Transfer\CreateTransferService;
 use App\Services\Transaction\Transfer\CreateTransferServiceInterface;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Queue;
 
 class CreateTransferServiceTest extends TestCase
 {
@@ -41,7 +43,7 @@ class CreateTransferServiceTest extends TestCase
     /** 
      * @test 
      */
-    public function testShouldNotFinishWhenTransactionIsNotCreatedInDatabase()
+    public function test_should_not_finish_when_transaction_is_not_created_in_database()
     {
         $this->expectException(CreateTransferException::class);
         $this->expectExceptionMessage('An error occurred while inserting transfer data on database');
@@ -60,18 +62,19 @@ class CreateTransferServiceTest extends TestCase
     /** 
      * @test 
      */
-    public function testShouldNotFinishWhenSubtractUserBalanceFail()
+    public function test_should_not_finish_when_subtract_user_balance_fail()
     {
         $this->expectException(CreateTransferException::class);
         $this->expectExceptionMessage('The user has no balance to proceed');
 
-        $transactionFake = Transaction::create([
-            'payee_id' => $this->payee->id,
-            'payer_id' => $this->payer->id,
-            'value' => 10
-        ]);
+        $this->transactionRepo->method('create')->willReturn(
+            Transaction::create([
+                'payee_id' => $this->payee->id,
+                'payer_id' => $this->payer->id,
+                'value' => 10
+            ])
+        );
 
-        $this->transactionRepo->method('create')->willReturn($transactionFake);
         $this->userRepo->method('subtractBalance')->willReturn(false);
 
         $createTransferService = new CreateTransferService(
@@ -81,5 +84,58 @@ class CreateTransferServiceTest extends TestCase
 
         /** @var CreateTransferServiceInterface*/
         $createTransferService->handle($this->payee->id, $this->payer->id, 10);
+    }
+
+    /**
+     * @test
+    */
+    public function test_should_create_transfer_job_process_when_finished()
+    {
+        Queue::fake();
+        $this->transactionRepo->method('create')->willReturn(
+            Transaction::create([
+                'payee_id' => $this->payee->id,
+                'payer_id' => $this->payer->id,
+                'value' => 10
+            ])
+        );
+        $this->userRepo->method('subtractBalance')->willReturn(true);
+
+        $createTransferService = new CreateTransferService(
+            $this->transactionRepo,
+            $this->userRepo
+        );
+
+        /** @var CreateTransferServiceInterface*/
+        $createTransferService->handle($this->payee->id, $this->payer->id, 10);
+        Queue::assertPushed(ProcessTransferJob::class);
+    }
+
+    /**
+     * @test
+     */
+    public function test_should_return_valid_transaction_when_finished()
+    {
+        $this->transactionRepo->method('create')->willReturn(
+            Transaction::create([
+                'payee_id' => $this->payee->id,
+                'payer_id' => $this->payer->id,
+                'value' => 10
+            ])
+        );
+
+        $this->userRepo->method('subtractBalance')->willReturn(true);
+
+        $createTransferService = new CreateTransferService(
+            $this->transactionRepo,
+            $this->userRepo
+        );
+
+        /** @var CreateTransferServiceInterface*/
+        $transaction = $createTransferService->handle($this->payee->id, $this->payer->id, 10);
+
+        $this->assertEquals(10, $transaction->value);
+        $this->assertEquals($this->payee->id, $transaction->payee_id);
+        $this->assertEquals($this->payer->id, $transaction->payer_id);
     }
 }
