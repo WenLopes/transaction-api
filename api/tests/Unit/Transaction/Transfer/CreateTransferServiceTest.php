4d9933822ca484614ba3f3785ceed52c66e48fb0
Wendel Lopes <wendel.lopes@telium.com.br>
2021-05-31 16:45:02 -0300

    Fixing initial tests

--- api/tests/Unit/Transaction/Transfer/CreateTransferServiceTest.php
+++ api/tests/Unit/Transaction/Transfer/CreateTransferServiceTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\Transaction\Transfer;
 
 use App\Exceptions\Transaction\Transfer\CreateTransferException;
+use App\Models\Transaction\Transaction;
 use App\Repositories\Transaction\TransactionRepository;
 use App\Repositories\Transaction\TransactionRepositoryInterface;
 use App\Repositories\User\UserRepository;
@@ -34,10 +35,11 @@ class CreateTransferServiceTest extends TestCase
     public function testShouldNotFinishWhenTransactionIsNotCreatedInDatabase($payer, $payee, $value)
     {
         $this->expectException(CreateTransferException::class);
+        $this->expectExceptionMessage('An error occurred while inserting transfer data on database');
 
-        $userRepo = $this->createMock(UserRepositoryInterface::class);
+        $userRepo = $this->createMock(UserRepository::class);
 
-        $transactionRepo = $this->createMock(TransactionRepositoryInterface::class);
+        $transactionRepo = $this->createMock(TransactionRepository::class);
         $transactionRepo->method('create')->willReturn(null);
 
         $createTransferService = new CreateTransferService(
@@ -56,11 +58,18 @@ class CreateTransferServiceTest extends TestCase
     public function testShouldNotFinishWhenSubtractUserBalanceFail($payer, $payee, $value)
     {
         $this->expectException(CreateTransferException::class);
+        $this->expectExceptionMessage('The user has no balance to proceed');
+
+        $transactionRepo = $this->createMock(TransactionRepositoryInterface::class);
+        $transactionFake = Transaction::create([
+            'payee_id' => $payee,
+            'payer_id' => $payer,
+            'value' => $value
+        ]);
+        $transactionRepo->method('create')->willReturn($transactionFake);
 
         $userRepo = $this->createMock(UserRepositoryInterface::class);
         $userRepo->method('subtractBalance')->willReturn(false);
-
-        $transactionRepo = $this->createMock(TransactionRepositoryInterface::class);
 
         $createTransferService = new CreateTransferService(
             $transactionRepo,
