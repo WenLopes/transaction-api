5891ddece384ab61242dab6acc3dd89407ddd639
Wendel Lopes <wendel.lopes@telium.com.br>
2021-06-01 19:01:15 -0300

    Creating feature test for show transaction

---
+++ api/tests/Feature/Transaction/ShowTransactionTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Transaction\Transaction;
+use App\Models\User\User;
+use Tests\TestCase;
+
+class ShowTransactionTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function test_should_return_transaction_with_valid_transaction_id()
+    {
+        $payer = User::factory()->create([
+            'is_seller' => false,
+            'balance' => 100,
+            'active' => true
+        ]);
+
+        $payee = User::factory()->create([
+            'is_seller' => false,
+            'balance' => 100,
+            'active' => true
+        ]);
+
+        $transaction = Transaction::factory()->create([
+            'payer_id'  => $payer->id,
+            'payee_id'  => $payee->id,
+            'value'     => 10,
+            'transaction_status_id' => 1
+        ]);
+
+        $response = $this->get("api/transaction/{$transaction->id}");
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function test_should_not_return_transaction_with_invalid_transaction_id()
+    {
+        $transactionInvalidId = intval(INF);
+        $response = $this->get("api/transaction/$transactionInvalidId");
+        $response->assertStatus(422);
+        $response->assertExactJson([
+            'errors' => [
+                'transaction' => [ 
+                    "The selected transaction is invalid."
+                ]
+            ]
+        ]);
+    }
+}
