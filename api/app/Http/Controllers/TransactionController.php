5cb2290834946efdbdf4083377546746e6ada0a4
Wendel Lopes <wendel.lopes@telium.com.br>
2021-06-01 16:36:56 -0300

    Creating transaction get method on controller  and request

--- api/app/Http/Controllers/TransactionController.php
+++ api/app/Http/Controllers/TransactionController.php
@@ -2,17 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\Transaction\NotFoundTransactionException;
 use App\Http\Requests\Transaction\CreateTransactionRequest;
+use App\Http\Requests\Transaction\ShowTransactionRequest;
 use App\Http\Resources\TransactionResource;
+use App\Repositories\Transaction\TransactionRepositoryInterface;
 use App\Services\Transaction\Transfer\CreateTransferServiceInterface;
 
 class TransactionController extends Controller
 {
+
+    public function show(
+        ShowTransactionRequest $request,
+        TransactionRepositoryInterface $transactionRepo
+    ) {
+        $transaction = $transactionRepo->findById( $request->route('transaction') );
+
+        if(!$transaction){
+            throw new NotFoundTransactionException();
+        }
+
+        return response()->json( new TransactionResource($transaction) );
+    }
+
     public function create(
         CreateTransactionRequest $request,
         CreateTransferServiceInterface $createTransferService
-    )
-    {
+    ) {
         $payload = $request->validated();
 
         [
