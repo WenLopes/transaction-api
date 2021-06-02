1d3d0e611c843dd24929601e23b1d43fbd48a4ca
Wendel Lopes <wendel.lopes@telium.com.br>
2021-06-01 21:34:46 -0300

    Creating transaction repository test

---
+++ api/tests/Unit/Repositories/TransactionRepositoryTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests\Unit\Repositories;
+
+use App\Models\Transaction\Transaction;
+use App\Repositories\Transaction\TransactionRepository;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class TransactionRepositoryTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /** @var TransactionRepository */
+    protected $transactionRepo;
+
+    protected function setUp() : void
+    {
+        parent::setUp();
+        $this->transactionRepo = app(TransactionRepository::class);
+    }
+
+    public function setAsSuccessDataProvider() {
+
+        $this->refreshApplication();
+
+        $transactionWaiting = Transaction::factory()->create();
+
+        $transactionSuccess = Transaction::factory()->create([
+            'transaction_status_id' => config('constants.transaction.status.SUCCESS')
+        ]);
+
+        return [
+
+            'invalid_transaction' => [ 
+                'transactionId' => intval(INF),
+                'expected' => false
+            ],
+
+            'transaction_in_waiting_status' => [ 
+                'transactionId' => $transactionWaiting->id,
+                'expected' => true
+            ],
+
+            'transaction_in_success_status' => [ 
+                'transactionId' => $transactionSuccess->id,
+                'expected' => false
+            ]
+        ];
+
+    }
+
+    /**
+     * @test
+     * @dataProvider setAsSuccessDataProvider
+     */
+    public function test_set_transaction_as_success(int $transactionId, bool $expected)
+    {
+        $query = $this->transactionRepo->setAsSuccess($transactionId);
+        $this->assertEquals($expected, $query);
+    }
+}
