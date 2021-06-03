b7c30e9d74a9aa7ee130dfd646e79fd6f5b5e3ce
Wendel Lopes <wendel.lopes@telium.com.br>
2021-06-02 21:17:14 -0300

    Updating transaction repository test

--- api/tests/Unit/Repositories/TransactionRepositoryTest.php
+++ api/tests/Unit/Repositories/TransactionRepositoryTest.php
@@ -23,37 +23,11 @@ class TransactionRepositoryTest extends TestCase
     /**
      * @test
      */
-    public function test_status_should_not_set_as_success_when_transaction_is_already_successful()
-    {
-        $transactionSuccess = Transaction::factory()->create([
-            'transaction_status_id' => config('constants.transaction.status.SUCCESS')
-        ]);
-
-        $query = $this->transactionRepo->setAsSuccess($transactionSuccess->id);
-        $this->assertEquals(false, $query);
-    }
-
-    /**
-     * @test
-     */
     public function test_status_should_be_success_when_transaction_is_waiting()
     {
         $transactionWaiting = Transaction::factory()->create();
         $query = $this->transactionRepo->setAsSuccess($transactionWaiting->id);
         $this->assertEquals(true, $query);
-    }
-
-    /**
-     * @test
-     */
-    public function test_status_should_not_set_as_error_when_transaction_is_already_error()
-    {
-        $transactionError = Transaction::factory()->create([
-            'transaction_status_id' => config('constants.transaction.status.ERROR')
-        ]);
-        
-        $query = $this->transactionRepo->setAsError($transactionError->id);
-        $this->assertEquals(false, $query);
     }
 
     /**
