81b7b702c894366d1d86868015f64ff631f36e6b
Wendel Lopes <wendel.lopes@telium.com.br>
2021-06-02 23:38:52 -0300

    Updating user repository test

--- api/tests/Unit/Repositories/UserRepositoryTest.php
+++ api/tests/Unit/Repositories/UserRepositoryTest.php
@@ -26,9 +26,9 @@ class UserRepositoryTest extends TestCase
     */
     public function test_should_not_create_users_with_duplicate_document()
     {
-        $randomString = "duplicate".strtotime(now());
         $this->expectException(QueryException::class);
         $this->expectExceptionMessageMatches("/UQ_Users_Document/i");
+        $randomString = "duplicate".strtotime(now());
 
         $originalUser = User::factory()->create();
         $this->userRepo->create([
@@ -53,51 +53,6 @@ class UserRepositoryTest extends TestCase
             'email' => $originalUser->email,
             'document' => '00000000000',
             'password' => 'password'
-        ]);
-    }
-
-    /**
-     * @test
-     */
-    public function test_should_not_subtract_balance_if_the_value_is_greater_than_it()
-    {
-        $this->expectException(\Exception::class);
-        $this->expectExceptionMessage("Value greater than available user balance");
-        $user  = User::factory()->create([
-            'balance' => 100
-        ]);
-        $this->userRepo->subtractBalance($user->id, 1000);
-    }
-
-    /**
-     * @test
-     */
-    public function test_should_subtract_balance()
-    {
-        $user  = User::factory()->create([
-            'balance' => 1000
-        ]);
-
-        $this->assertTrue($this->userRepo->subtractBalance($user->id, 100));
-        $this->assertDatabaseHas('users', [
-            'id' => $user->id,
-            'balance' => 900
-        ]);
-    }
-
-    /**
-     * @test
-     */
-    public function test_should_add_balance()
-    {
-        $user  = User::factory()->create([
-            'balance' => 100
-        ]);
-
-        $this->assertTrue($this->userRepo->addBalance($user->id, 100));
-        $this->assertDatabaseHas('users', [
-            'id' => $user->id,
-            'balance' => 200
         ]);
     }
 }
